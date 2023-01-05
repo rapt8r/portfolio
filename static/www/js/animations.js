d9febb093c009c5b26fd504841788f60c27af443
Dominik Ciupa <20306035+rapt8r@users.noreply.github.com>
2023-01-05 14:53:15 +0100

    About me section added, footer added, projects section added

--- static/www/js/animations.js
+++ static/www/js/animations.js
@@ -8,17 +8,20 @@ else {
 }
 let wavingHandShakeAnimation = anime({
     targets: '#waving-hand-icon',
-    rotate: 15,
+    rotate: 25,
+    translateY: -20,
     autoplay: false,
     direction: 'alternate',
+    duration: 1000,
     loop: true,
     easing: 'easeInOutSine',
 });
 
+
 let wavingHandInAnimation = anime({
     targets: '#waving-hand-icon',
     keyframes: [
-        {translateY: [-50, 0], opacity: [0, 1], duration: 1500},
+        {translateY: [-70, 0], opacity: [0, 1], duration: 1500},
     ],
     complete: function () {
         wavingHandShakeAnimation.play();
